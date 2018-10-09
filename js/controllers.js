ad446e2d9562ea207ddaceb8b6717920641868ec
owlvano <owlvano@gmail.com>
2018-10-10 00:31:15 +0300

    matches view commit

--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,7 @@ var FootballControllers = angular.module('FootballControllers', []);
 
 FootballControllers.controller('CompetitionsController', ['$scope', '$http',  
     function($scope, $http){
+        $scope.availableCompetitionIds = [2000,2001,2002,2003,2013,2014,2015,2016,2017,2018,2019,2021];
         $http({
             url: 'http://api.football-data.org/v2/competitions/',
             method: 'GET',
